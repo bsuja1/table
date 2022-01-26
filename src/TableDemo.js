edf8b4422960fb26d6f155b4e5cfb799e603c319
bsuja1 <bsuja1@users.noreply.github.com>
2022-01-25 19:32:20 -0800

    edit delete functions

--- src/TableDemo.js
+++ src/TableDemo.js
@@ -5,25 +5,8 @@ import {
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-// Creating styles
-const useStyles = makeStyles({
-	root: {
-		"& > *": {
-			borderBottom: "unset",
-		},
-	},
-	table: {
-		minWidth: 650,
-	},
-	snackbar: {
-		bottom: "104px",
-	},
-});
 
 function TableDemo() {
-	// Creating style object
-	const classes = useStyles();
-
 	// Defining a state named rows
 	// which we can update by calling on setRows function
 	const [rows, setRows] = useState([
@@ -32,120 +15,153 @@ function TableDemo() {
 
 	// Initial states
     const [state, setState] = useState('');
-	const [isEdit, setEdit] = React.useState(false);
-
-
+	const [edit, setEdit] = useState(false); //flag
+	const [editRow, setEditRow] = useState(0);//stores row being edited
+	const [editedMsg, setEditedMsg] = useState("");//stores new edited message
+	
 	// The handleInputChange handler can be set up to handle
 	// many different inputs in the form, listen for changes
 	// to input elements and record their values in state
 	const handleInputChange = (e) => {
-		e.preventDefault();
         //console.log(e.target.value)
         setState(e.target.value)
 	};
 
     // Function For adding new row object
 	const handleAdd = (e) => {
-		e.preventDefault();
+		//e.preventDefault();
         const text = state.trim();
 		if (text.length>0)
 		{
 			setRows([...rows,{message: state}])
 			setState('')
+			setEdit(false);
 		}
 	};
 
 	// Function to handle edit
 	const handleEdit = (i) => {
-		console.log("onEdit")
-		// If edit mode is true setEdit will
-		// set it to false and vice versa
+		//console.log("onEdit")
+		//Save the row number and row message in variables
+		setEditedMsg(rows[i].message)
+		setEditRow(i)
+		//set flag to indicate edit in progress
+		setEdit(true);
+	};
+	
+	// Function to handle edit
+	const handleEditChange = (e) => {
+		//console.log("onEditChange")
+		setEditedMsg(e.target.value)
 	};
 
-	// Handle the case of delete confirmation where
-	// user click yes delete a specific row of id:i
+	const handleUpdate = (i) => {		
+		//console.log("onUpdate", editedMsg)
+		const text = editedMsg.trim();
+		if (text.length>0)
+		{
+			const list = [...rows];
+			list[i].message=editedMsg;
+			setRows(list);
+			//reset flag, row number and row message
+			setEditRow(0)
+			setEditedMsg("")
+			setEdit(false)
+		}		
+	};
+
+	// Handle the case of delete 
 	const handleDelete = (i) => {
-		console.log("onDelete")
-		/*const list = [...rows];
+		//console.log("onDelete")
+		const list = [...rows];
 		list.splice(i, 1);
-		setRows(list);
-		*/
-		
+		setRows(list);		
+	};
+
+	// Handle the case of reply 
+	const handleReply = (i) => {
+		console.log("onReply")	
 	};
 
 return (
-	<TableBody>
-	<Box margin={1}>
-		<div style={{ display: "flex", justifyContent: "space-between" }}>
-            <div>
-                <TableCell padding="none">
-                    <input
-                        type="text"
-                        placeholder="Start a new message..."
-                        value={state}
-                        onChange={handleInputChange}
-                    />
-                </TableCell>
-                <TableCell padding="none">
-                    <Button onClick={(e) => handleAdd(e)}>
-                        Send Message
-                    </Button>			
-                </TableCell>           
-            </div>
-		</div>
-		<TableRow align="center"></TableRow>
-
-		<Table
-		className={classes.table}
-		size="medium"
-		aria-label="a dense table"
-		>
+	<Box margin={5}>
+		<TableCell>
+			<input
+				type="text"
+				size="50"
+				placeholder="Start a new message..."
+				value={state}
+				onChange={handleInputChange}
+			/>
+		</TableCell>
+		<TableCell>
+			<Button onClick={(e) => handleAdd(e)}>Send Message</Button>			
+		</TableCell>           
+		<Table>
 		<TableHead>
 			<TableRow>
-			<TableCell>Messages</TableCell>
+				Messages                    
 			</TableRow>
 		</TableHead>
-		{(rows.length>0)
+		{(rows.length>1)
         ?
-        <div>
         <TableBody>
-            <div>
 			{rows.map((row, i) => {
 			return (
-				<div>
 				<TableRow>
 					{(row.message.length>0)
 					?
-					<div>
-						<TableCell padding="none">
-					{		row.message}
-						</TableCell>
-						<TableCell>
-							<button onClick={handleEdit(i)} name="button_edit">Edit</button>
-							<button onClick={handleDelete(i)} name="button_delete">Delete</button>
-							<button name="button_reply">Reply</button>
-						</TableCell>
-					</div>
+						<TableRow>
+							{((edit) && (editRow===i))
+							?
+							<div>		
+								<TableCell width ="50">
+								<input
+									name="edit_input"
+									type="text"
+									value={editedMsg}
+									onChange= {handleEditChange}
+								/>
+								</TableCell>
+								<TableCell width ="50">
+									<button onClick={handleUpdate.bind(this,i)} name="button_update">Update</button>
+								</TableCell>
+							
+							</div>
+							:
+							<div>
+							<TableCell width ="50">
+								{row.message}
+							</TableCell>
+							<TableCell>
+								<button onClick={handleEdit.bind(this,i)} name="button_edit">Edit</button>
+								<button onClick={handleDelete.bind(this,i)} name="button_delete">Delete</button>
+								<button onClick={handleReply.bind(this,i)}name="button_reply">Reply</button>
+							</TableCell>
+							</div>
+							}
+						</TableRow>
 					:
-					<div>
-
-					</div>
+					<TableCell>
+					</TableCell>
 					}
 				</TableRow>
-				</div>
 			);
 			})}
-            </div>
 		</TableBody>
-        </div>
         :
-        <div>
-            No messages yet...
-        </div>
+        <TableBody>
+			<TableRow></TableRow>
+			<TableRow>
+				<TableCell>
+					No messages yet...
+				</TableCell>
+			</TableRow>
+		</TableBody>
         }
 		</Table>
 	</Box>
-	</TableBody>
+	
 );
 }
 
